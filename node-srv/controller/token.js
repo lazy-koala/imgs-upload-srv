d13f0e2f70518e2815006f17471bb32272e10068
Acexy <acexy@thankjava.com>
2018-12-21 20:06:51 +0800

    bugfix: #6

--- node-srv/controller/token.js
+++ node-srv/controller/token.js
@@ -19,11 +19,13 @@ module.exports = new Router(
     let tokens = await authTokenModel.selectByUserIdSortByCreateTimeDesc(userId);
     if (tokens) {
         let now = Date.now();
+        let expirationTokenId = [];
         let expirationToken = [];
         for (let index in tokens) {
             tokens[index].token = tokens[index].token.replace(baseController.REG.TOKEN_ENCODE, '$1******$2');
             if (now >= tokens[index].expiration) {
-                expirationToken.push(tokens[index]._id.toString());
+                expirationTokenId.push(tokens[index]._id.toString());
+                expirationToken.push(tokens[index].token);
                 // 过期token移除
                 tokens.splice(index, 1);
                 continue;
@@ -31,8 +33,11 @@ module.exports = new Router(
             delete tokens[index].userId;
         }
 
-        if (expirationToken.length > 0) {
-            await authTokenModel.removeOwnByIds(expirationToken, userId);
+        if (expirationTokenId.length > 0) {
+            await authTokenModel.removeOwnByIds(expirationTokenId, userId);
+            for (let index in expirationToken) {
+                await asyncRedisClient.delAsync(redisKey.AUTH_TOKEN(expirationToken[index].token));
+            }
         }
 
         return baseController.response(ctx, {list: tokens});
