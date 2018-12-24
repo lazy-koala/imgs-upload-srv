2f679e6651a107f72f885cd6a11e3157b6afcb22
Acexy <thankjava@foxmail.com>
2018-12-24 11:13:47 +0800

    调整了可配置部分uri不打印出入参数, 敏感信息不予输出

---
+++ node-srv/const/unlogPathname.js
@@ -0,0 +1,11 @@
+/**
+ * 不允许系统记录出入参数日志的链接
+ * @type {string[]}
+ */
+module.exports = [
+    '/login',
+    '/registe',
+    '/forget_pwd',
+    '/reset_pwd',
+    '/view',
+];
--- node-srv/middleware/basic.js
+++ node-srv/middleware/basic.js
@@ -4,6 +4,10 @@
  * @Description:基础中间件
  */
 
+const unlogPathname = require('../const/unlogPathname');
+const url = require('url');
+
+
 module.exports = async (ctx, next) => {
 
     let startTime = beforeController(ctx);
@@ -20,9 +24,10 @@ const beforeController = ctx => {
         requestContent = ctx.request.body ? JSON.stringify(ctx.request.body) : 'null';
     }
 
-    // 防止敏感信息打印 取消当前日志输出
-    let logInfo = '=> koa: '.cyan + '<<< requestContent = '.grey + requestContent.blue + ' | url = '.grey + ctx.url.blue;
-    console.log(logInfo);
+    if (!unlog(url.parse(ctx.url, true).pathname)) {
+        let logInfo = '=> koa: '.cyan + '<<< requestContent = '.grey + requestContent.blue + ' | url = '.grey + ctx.url.blue;
+        console.log(logInfo);
+    }
 
     return Date.now();
 };
@@ -39,10 +44,20 @@ const afterController = (ctx, startTime) => {
         ctx.body = {message: '405 Method Not Allowed'};
     }
 
-    // 防止敏感信息打印 取消当前日志输出
-    let logInfo = '=> koa: '.cyan + '>>> responseContent = '.grey + '%s' + ' | url = '.grey + '%s'.blue;
-    logInfo += ' | status = '.grey + '%s'.blue + ' | costTime = '.grey + '%s'.blue + ' ms'.grey;
+    if (!unlog(url.parse(ctx.url, true).pathname))  {
+        let logInfo = '=> koa: '.cyan + '>>> responseContent = '.grey + '%s' + ' | url = '.grey + '%s'.blue;
+        logInfo += ' | status = '.grey + '%s'.blue + ' | costTime = '.grey + '%s'.blue + ' ms'.grey;
 
-    console.log(logInfo, ctx.body ? JSON.stringify(ctx.body).blue : 'no data response'.red, ctx.url, ctx.response.status, Date.now() - startTime);
+        console.log(logInfo, ctx.body ? JSON.stringify(ctx.body).blue : 'no data response'.red, ctx.url, ctx.response.status, Date.now() - startTime);
+    }
 
+};
+
+const unlog = pathname => {
+    for (let index in unlogPathname) {
+        if (pathname.startsWith(unlogPathname[index])) {
+            return true;
+        }
+    }
+    return false;
 };
