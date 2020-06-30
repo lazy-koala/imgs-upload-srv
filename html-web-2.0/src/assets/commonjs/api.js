01d271c5b21bfdf6696f04cabf76a70bb0b6cdcb
Acexy <acexy@thankjava.com>
2020-06-30 16:28:44 +0800

    调整目录名

--- html-web-2.0/src/assets/commonjs/api.js
+++
@@ -1,4 +0,0 @@
-import http from './onloadF.js';
-
-// export const getUserInfo = params => http.get('', params);
-export const logout = params => http.get('/api/logout', params);
--- html-web-2.0/src/views/Person.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        的点点滴滴
-    </div>
-</template>
