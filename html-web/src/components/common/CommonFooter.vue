333326b32ea63fca60d9b58c62bd2ab311b00991
yangzhiyuan <yzhuiyi@126.com>
2021-01-29 10:18:09 +0800

    底部日期获取更改

--- html-web/src/components/common/CommonFooter.vue
+++ html-web/src/components/common/CommonFooter.vue
@@ -1,12 +1,18 @@
 <template>
 <div class="footer">
     <!-- <a href="https://github.com/lazy-koala/"><img src="https://img.shields.io/badge/org-@LazyKoala-yellow.svg" alt="org" data-canonical-src="https://img.shields.io/badge/org-@LazyKoala-yellow.svg" style="max-width:70%;"></a> -->
-    <span>&copy;Copyright (C) 2018-2020 All rights reserved</span>
+    <span>{{`&copy;Copyright (C) 2018-${getFullYear()} All rights reserved`}}</span>
 </div>
 </template>
 <script type="text/javascript">
     export default {
-        name: 'CommonFooter'
+        name: 'CommonFooter',
+        methods: {
+            getFullYear() {
+                let date = new Date();
+                return date.getFullYear();
+            }
+        }
     }
 </script>
 <style type="text/css" scoped>
